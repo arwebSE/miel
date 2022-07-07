69dae7bc95641e095058a1a1f789618438e2ddd1
August R <aggesoft@gmail.com>
2022-07-07 15:55:45 +0200

    tennis

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,26 @@
-import './index.css';
+import "./index.css";
+const config = require("../config.json");
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const cityInput = document.getElementById("cityInput");
+const callButton = document.getElementById("callAPI");
+const weatherEl = document.getElementById("weather");
+const tempEl = document.getElementById("temp");
+const saveButton = document.getElementById("save");
+
+const callAPI = async (city) => {
+    const response = await fetch(`${config.weatherURL}?q=${city}&appid=${config.apiKey}&units=metric`);
+    const result = await response.json();
+    console.log("callAPI", result);
+    console.log(`City: ${result.name} Temp: ${result.main.temp}`);
+    weatherEl.innerHTML = `City: ${result.name} Temp: ${result.main.temp}`;
+    tempEl.innerHTML = result.main.temp;
+};
+
+callButton.onclick = () => {
+    callAPI(cityInput.value);
+    console.log("saved", store.get("city"));
+};
+
+saveButton.onclick = () => {
+    window.saveCity();
+};
